fee433e8cea1c0f3e3842e0f87f40a666f525e31
Fredrick-Okori <109782974+Fredrick-Okori@users.noreply.github.com>
2026-06-18 13:31:05 +0300

    adding optimimzation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
@@ -11,9 +12,6 @@ const nextConfig = {
                 hostname: 'btmwaweqwxwmmfscnzbe.supabase.co',
             },
         ],
-        formats: ['image/avif', 'image/webp'],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     },
     experimental: {
         optimizeCss: true,
